bfe15787e3ce26d1a72a9090ef576773bb2b2841
gyojincompany <luckyred@gmail.com>
2023-04-07 13:01:53 +0900

    80미만 조건 추가 v1.0.1

--- src/exer/TestJava.java
+++ src/exer/TestJava.java
@@ -13,8 +13,10 @@ public class TestJava {
 			System.out.println("두번째 조건의 결과가 참입니다!!");
 		} else if(score < 70) {
 			System.out.println("세번째 조건의 결과가 참입니다!!");
+		} else if(score < 80) {
+			System.out.println("네번째 조건의 결과가 참입니다!!");//80미만 조건 추가
 		} else if(score < 90) {
-			System.out.println("네번째 조건의 결과가 참입니다!!");
+			System.out.println("다섯번째 조건의 결과가 참입니다!!");
 		} else {
 			System.out.println("모든 조건의 결과가 거짓입니다!!");
 		}
